264131d806e3e36babf0be182c95027127347134
Azim Sofi <89006182+AzimSofi@users.noreply.github.com>
2023-07-31 23:21:05 +0800

    Template
    https://reitaisai.com/ コピーした

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <h3><a href="#" style="color: #f1e2dc;">タイトル</a></h3>
+    </header>   
+    <div class='container1'>
+        <div class='item1'>
+            <iframe width="840px" height="472.5px" src="https://www.youtube.com/embed/HkIQDx3GVCs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+        <div class='item1 item11'>
+                <div style="margin-left: 30px; font-size: 25px;">
+                    作成
+                </div>
+        </div>
+        <div class='container2'>
+            <div class='item1 item12'>
+                <a href="#">
+                    <img class="pop-out-image" src="画像\COMITIA126 sample.jpg" alt='COMITIA126 sample.jpg' title='絵' width='420px'>
+                </a>
+                <div class='item12_caption'>
+                    などなど<br>
+                </div>
+            </div>
+            <div class='item1'>
+                <!--<img src="画像\COMITIA126 sample.jpg" alt='COMITIA126 sample.jpg' title='絵' width='300px'>
+                -->
+            </div>
+        </div>
+        <div class='item1 item11'>
+                <div style="margin-left: 30px; font-size: 25px;">
+                    作成
+                </div>
+        </div>
+        <div class='container3'>
+            <div class='test_item'></div>
+            <div class='test_item'></div>
+            <div class='test_item'></div>
+            <div class='test_item'></div>
+            <div class='test_item'></div>
+            <div class='test_item'></div>
+        </div>
+    </div>
+</body>
+</html>
